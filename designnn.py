93aa3524e5563c2296486d9b95034c8a25ae3d4c
mohamed walid <M.walid2376@nu.edu.eg>
2026-04-01 23:01:10 +0200

    second line edit

---
+++ designnn.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df= pd.read_excel("excel idea.xlsx")
+
+d= df["Depth"][0]-50
+Mu= df["Moment"][0]*1e6
+
+R= Mu/(df["Width"][0]*d**2)
+
+if R<4: 
+   Ratio=0.0055
+else:
+   Ratio= 0.0065
+   
