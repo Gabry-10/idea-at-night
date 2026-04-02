0508b679c3a83be8d19691af323ea5d041ee741e
mohamed walid <M.walid2376@nu.edu.eg>
2026-04-02 14:27:37 +0200

    final

--- designnn.py
+++ designnn.py
@@ -1,21 +1,22 @@
 import pandas as pd
+df = pd.read_excel("excel idea.xlsx")
 
-df= pd.read_excel("excel idea.xlsx")
+b= df["Width"][0]
+h= df["Depth"][0]
+fy= df["fy"][0]
+fcu = df["fcu"][0]
+Mu = df["Moment"][0]
 
-d= df["Depth"][0] -50
-Mu= df["Moment"][0]*1e6
+cover = 50
+d = h - cover
 
-R= Mu/(df["Width"][0]*d**2)
+R = Mu / (b * d**2)
+
 if R<4:
-    Ratio= 0.0055
+    ratio = 0.0055
 else:
-    Ratio= 0.0065
-As= Ratio  * df["Width"][0]*d
-df["As"]=As
-
-df.to_csv("results.csv",index=False)
-
-print("As=",As)
-
-
-print("Done")
+    ratio = 0.0065
+As = ratio * b * d
+df["As"] = As 
+df.to_csv("result.csv",index=False)
+print("done! check result in csv file")   
